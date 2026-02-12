969211ebfc2c8a243bb5e8eaf6dfd13a7e674951
AlaaAlshafey1 <alaa.alshafey12345@gmail.com>
2026-02-12 13:33:41 +0200

    last enhanc on filters

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer opened -->
 <div class="main-footer ht-40">
 	<div class="container-fluid pd-t-0-f ht-100p">
-		<span>Copyright © 2020 <a href="#">Alameen</a>. All rights reserved.</span>
+		<span>Copyright © 2026 <a href="#">Alameen</a>. All rights reserved.</span>
 	</div>
 </div>
 <!-- Footer closed -->
